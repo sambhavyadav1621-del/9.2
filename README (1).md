309ef0d50b1184f47383e58a59edbc12f19bfe5c
sambhavyadav1621-del <sambhavyadav1621@gmail.com>
2025-11-02 19:40:15 +0530

    Add files via upload

---
+++ README (1).md
@@ -0,0 +1,2 @@
+# 9.2
+Set Up CI/CD Pipeline using GitHub Actions
---
+++ prac9.2.1.sql
@@ -0,0 +1,9 @@
+your-repo/
+├── .github/
+│   └── workflows/
+│       └── main.yml
+├── package.json
+├── src/
+│   └── App.js
+└── public/
+    └── index.html
